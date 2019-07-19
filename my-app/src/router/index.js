c872b6667446ac24491cd0853e532adf96a882b8
healer050 <575969679@qq.com>
2019-07-19 12:01:00 +0800

    wf

---
+++ my-app/src/router/index.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '../pages/index'
+import Denglu from '../pages/denglu'
+import Denglutwo from '../pages/denglutwo'
+import Register from '../pages/register'
+import Mine from '../pages/mine'
+import Carshop from '../pages/carshop'
+import Fenlei from '../pages/fenlei'
+import Hequlife from '../pages/Hequlife'
+
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/hequlife',
+      name: 'Hequlife',
+      component: Hequlife
+    },
+    {
+      path: '/index',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/mine',
+      name: 'Mine',
+      component: Mine
+    },
+    {
+      path: '/carshop',
+      name: 'Carshop',
+      component: Carshop
+    },
+    {
+      path: '/fenlei',
+      name: 'Fenlei',
+      component: Fenlei
+    },
+    {
+      path: '/denglu',
+      name: '/Denglu',
+      component: Denglu
+    },
+    {
+      path: '/denglutwo',
+      name: '/Denglutwo',
+      component: Denglutwo
+    },
+    {
+      path: '/register',
+      name: '/Register',
+      component: Register
+    },
+    {path:'/*',redirect:"/index"},
+
+
+  ]
+})
